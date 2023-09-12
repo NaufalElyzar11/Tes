4013c38fad6f8c502aa9960493f460aa7c337cd1
Naufal Elyzar <144782193+NaufalElyzar11@users.noreply.github.com>
2023-09-12 16:49:52 +0800

    Add files via upload

---
+++ Python Naufal Elyzar 2310817210019.py
@@ -0,0 +1,4 @@
+#Bismillah
+print('Naufal Elyzar 2310817210019') #Kode untuk menghasilkan output berupa nama saya dan NIM saya
+print('Integrated development environment (IDE) adalah aplikasi perangkat lunak yang membantu para pemrogram mengembangkan kode perangkat lunak secara efisien. Aplikasi ini meningkatkan produktivitas developer dengan menggabungkan kemampuan seperti pengeditan, pembangunan, pengujian, dan pengemasan perangkat lunak dalam aplikasi yang mudah digunakan. Layaknya para penulis yang menggunakan editor teks dan para akuntan yang menggunakan spreadsheet, developer perangkat lunak menggunakan IDE untuk memudahkan pekerjaan mereka')
+print('Contohnya adalah Visual Studio Code (VSCode), Thonny ide, Arduino IDE, CLion') 
